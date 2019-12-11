0b0bd8eb7273831fc8a9dd49ab78fbfab75274d1
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-12-11 05:18:39 -0300

    Installed uikitten; added AppProvider

--- cleaver/index.js
+++ cleaver/index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import Home from './src/pages/Home';
 import {name as appName} from './app.json';
+import {ApplicationProvider} from '@ui-kitten/components';
+import {mapping, light as lightTheme} from '@eva-design/eva';
 
-AppRegistry.registerComponent(appName, () => Home);
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <Home />
+  </ApplicationProvider>
+);
+
+AppRegistry.registerComponent(appName, () => App);
