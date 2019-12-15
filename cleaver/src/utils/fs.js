9d24623271811bd21170fcb1988f6000646db4fc
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-12-14 21:02:08 -0300

    prevent picking unsupported file type

--- cleaver/src/utils/fs.js
+++ cleaver/src/utils/fs.js
@@ -1,6 +1,17 @@
 import FilePicker from 'react-native-file-picker';
 import {toast} from './sysUtils';
 
+const isSupportedFileType = filePath => {
+  const splited = filePath.split('.');
+  const fileType = splited[splited.length - 1];
+  return (
+    fileType === 'mp4' ||
+    fileType === 'avi' ||
+    fileType === 'flv' ||
+    fileType === 'mov'
+  );
+};
+
 const showFilePicker = () => {
   return new Promise((resolve, reject) => {
     FilePicker.showFilePicker(null, response => {
@@ -11,6 +22,11 @@ const showFilePicker = () => {
         toast(response.error);
         reject(response.error);
       } else {
+        if (!isSupportedFileType(response.path)) {
+          return toast(
+            'Por favor, selecione um arquivo de vídeo em um dos formatos suportados!',
+          );
+        }
         resolve(`file://${response.path}`);
       }
     });
