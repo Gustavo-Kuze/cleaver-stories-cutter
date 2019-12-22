15dcda05a0c665c1c65640df25fa404c63421815
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-12-22 08:57:01 -0300

    removed all cleaver related files

--- cleaver/src/components/VideoFormatsPicker/styles.js
+++
@@ -1,9 +0,0 @@
-import {Dimensions} from 'react-native';
-
-const styles = {
-  picker: {
-    width: Dimensions.get('screen').width - 20,
-  },
-};
-
-export default styles;
--- cleaver/src/utils/sysUtils.js
+++
@@ -1,5 +0,0 @@
-import {ToastAndroid} from 'react-native';
-const toast = (msg, duration = ToastAndroid.LONG) =>
-  ToastAndroid.show(msg, duration);
-
-export {toast};
